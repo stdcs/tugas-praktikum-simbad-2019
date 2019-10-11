30af70ceb8d4244fbffdd0e9bc4cb0f443e674d6
Fathur Fahrizal <fathurfahrizal9@gmail.com>
2019-10-12 01:24:08 +0800

    Tugas Praktikum 1

---
+++ H071181316/Praktikum_1/H071181316_fathurfahrizal.sql
@@ -0,0 +1,178 @@
+-- MariaDB dump 10.17  Distrib 10.4.6-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: skripsi
+-- ------------------------------------------------------
+-- Server version	10.4.6-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `dosen_pembimbing`
+--
+
+LOCK TABLES `dosen_pembimbing` WRITE;
+/*!40000 ALTER TABLE `dosen_pembimbing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dosen_pembimbing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `dosen_penguji`
+--
+
+LOCK TABLES `dosen_penguji` WRITE;
+/*!40000 ALTER TABLE `dosen_penguji` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dosen_penguji` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `mahasiswa_prodi`
+--
+
+LOCK TABLES `mahasiswa_prodi` WRITE;
+/*!40000 ALTER TABLE `mahasiswa_prodi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mahasiswa_prodi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `seminar_mahasiswa`
+--
+
+LOCK TABLES `seminar_mahasiswa` WRITE;
+/*!40000 ALTER TABLE `seminar_mahasiswa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seminar_mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `skripsi_mahasiswa`
+--
+
+LOCK TABLES `skripsi_mahasiswa` WRITE;
+/*!40000 ALTER TABLE `skripsi_mahasiswa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `skripsi_mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tdosen`
+--
+
+LOCK TABLES `tdosen` WRITE;
+/*!40000 ALTER TABLE `tdosen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tdosen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tmahasiswa`
+--
+
+LOCK TABLES `tmahasiswa` WRITE;
+/*!40000 ALTER TABLE `tmahasiswa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tmahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tnilai`
+--
+
+LOCK TABLES `tnilai` WRITE;
+/*!40000 ALTER TABLE `tnilai` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tnilai` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tpembimbing`
+--
+
+LOCK TABLES `tpembimbing` WRITE;
+/*!40000 ALTER TABLE `tpembimbing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tpembimbing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tpenguji`
+--
+
+LOCK TABLES `tpenguji` WRITE;
+/*!40000 ALTER TABLE `tpenguji` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tpenguji` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tprodi`
+--
+
+LOCK TABLES `tprodi` WRITE;
+/*!40000 ALTER TABLE `tprodi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tprodi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tsemester`
+--
+
+LOCK TABLES `tsemester` WRITE;
+/*!40000 ALTER TABLE `tsemester` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tsemester` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tseminar`
+--
+
+LOCK TABLES `tseminar` WRITE;
+/*!40000 ALTER TABLE `tseminar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tseminar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tsidang`
+--
+
+LOCK TABLES `tsidang` WRITE;
+/*!40000 ALTER TABLE `tsidang` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tsidang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tsk_pembimbing`
+--
+
+LOCK TABLES `tsk_pembimbing` WRITE;
+/*!40000 ALTER TABLE `tsk_pembimbing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tsk_pembimbing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tskripsi`
+--
+
+LOCK TABLES `tskripsi` WRITE;
+/*!40000 ALTER TABLE `tskripsi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tskripsi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `untitled`
+--
+
+LOCK TABLES `untitled` WRITE;
+/*!40000 ALTER TABLE `untitled` DISABLE KEYS */;
+/*!40000 ALTER TABLE `untitled` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-12  1:15:48
